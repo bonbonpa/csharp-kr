0546502e939ae393011cc39772228f156f1a08a9
bonbonpa <darksphin@gmail.com>
2017-02-16 21:55:53 +0700

    end sec 5

--- kr_csharp/Program.cs
+++ kr_csharp/Program.cs
@@ -10,22 +10,43 @@ namespace kr_csharp
     {
         static void Main(string[] args)
         {
-            int result = 0;
-            //sec 4 input output
-            Console.Write("Please input here year : ");
-            string inputd = Console.ReadLine();
-            try
-            {
-                int.TryParse(inputd, out result);
-                result += 543;
-            }
-            catch
-            {
+            #region sec 4 input output
+            //int result = 0;
+            ////sec 4 input output           
+            //Console.Write("Please input here year : ");
+            //string inputd = Console.ReadLine();
+            //try
+            //{
+            //    int.TryParse(inputd, out result);
+            //    result += 543;
+            //}
+            //catch
+            //{
 
-            }
+            //}
+            //Console.WriteLine("Hello :" + inputd);
+            //Console.WriteLine("Hello Year:"  + result);
+            #endregion
+            //sec 5 IF Structure
+            #region sec 5 IF Structure
+            //int score;
+            //Console.Write("Input your score");
+            //int.TryParse(Console.ReadLine(), out score);
+            //if (score >= 80 && score <= 100)
+            //{
+            //    Console.Write("Your Pass !!!");
+            //}
+            //else if(score >= 60 || score < 80)
+            //{
+            //    Console.Write("Your Grade B+");
+            //}
+            //else
+            //{
+            //    Console.Write("Not Pass");
+            //}
 
-            Console.WriteLine("Hello :" + inputd);
-            Console.WriteLine("Hello Year:"  + result);
+            #endregion
+
             Console.ReadKey();
         }
     }
