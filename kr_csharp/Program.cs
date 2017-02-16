6bf596541627e38ec4d4356c3a7ca587c65babdd
bonbonpa <darksphin@gmail.com>
2017-02-16 21:35:57 +0700

    sec 4.1

--- kr_csharp/Program.cs
+++ kr_csharp/Program.cs
@@ -11,22 +11,10 @@ namespace kr_csharp
         static void Main(string[] args)
         {
             //sec 4 input output
-            Console.WriteLine("Hello KR");
-            Console.ReadLine();
-
-            int number = 10;
-            string num = "string";
-            double db = 10.33;
-            float fl = 100;
-            bool gg = true;
-            string td = "111111";
-
-            Console.WriteLine("Please any key.... {0} age={1}..{2} " , number,fl,td );
-
+            Console.Write("Please input here : ");
+            string inputd = Console.ReadLine();
+            Console.WriteLine("Hello :"  + inputd);
             Console.ReadKey();
-
-        
-
         }
     }
 }
