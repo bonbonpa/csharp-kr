4fe6980ff2c8058287cfb6de2156d76953dc9c0d
bonbonpa <darksphin@gmail.com>
2017-02-16 21:19:08 +0700

    sec 2 end

--- kr_csharp/Program.cs
+++ kr_csharp/Program.cs
@@ -10,6 +10,21 @@ namespace kr_csharp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello KR");
+            Console.ReadLine();
+
+            int number = 10;
+            string num = "string";
+            double db = 10.33;
+            float fl = 100;
+            bool gg = true;
+
+            Console.WriteLine("MyName" + num);
+            Console.WriteLine("Grade" + number);
+            Console.WriteLine("Ages" + fl);
+            Console.WriteLine("status" + gg);
+
+            Console.ReadKey();
         }
     }
 }
